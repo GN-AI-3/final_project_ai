b8201e42149a1b0a7e92eaf61cada2e902aa5c57
JIHYE <jihyeoh91@gmail.com>
2025-03-31 16:18:57 +0900

    refactor: 폴더 구조 변경 및 ChatOpenAI 임포트 경로 수정

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,5 +1,5 @@
 from typing import Dict, Any
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 
 class BaseAgent:
