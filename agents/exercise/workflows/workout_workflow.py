2309d02876df344dd5358bed67026d3d0e47da72
jihxn-kim <lizars4024@gmail.com>
2025-04-10 16:04:15 +0900

    Fix: 운동 agent 계획 수립 단계에서 잘못된 json 전달 하던 오류 수정

--- agents/exercise/workflows/workout_workflow.py
+++ agents/exercise/workflows/workout_workflow.py
@@ -5,6 +5,7 @@ from ..nodes.exercise_routine_node import exercise_routine_agent
 from ..nodes.exercise_form_node import exercise_form_agent, confirm_exercise_form_agent
 from ..nodes.exercise_type_node import exercise_type_agent
 from ..nodes.exercise_planning_node import planning
+from ..nodes.exercise_execute_node import execute_plan
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
@@ -14,18 +15,20 @@ def create_workout_workflow():
     """운동 워크플로우 생성"""
     llm = ChatOpenAI(
         model="gpt-4o-mini",
-        temperature=0.0
+        temperature=0.7
     )
 
     workflow = StateGraph(RoutingState)
 
     # 노드 추가
-    workflow.add_node("routing", lambda state: routing(state, llm))
-    workflow.add_node("exercise_routine", lambda state: exercise_routine_agent(state, llm))
-    workflow.add_node("exercise_form", lambda state: exercise_form_agent(state, llm))
-    workflow.add_node("exercise_type", lambda state: exercise_type_agent(state, llm))
-    workflow.add_node("confirm_exercise_form", lambda state: confirm_exercise_form_agent(state, llm))
+    # workflow.add_node("routing", lambda state: routing(state, llm))
+    # workflow.add_node("exercise_routine", lambda state: exercise_routine_agent(state, llm))
+    # workflow.add_node("exercise_form", lambda state: exercise_form_agent(state, llm))
+    # workflow.add_node("exercise_type", lambda state: exercise_type_agent(state, llm))
+    # workflow.add_node("confirm_exercise_form", lambda state: confirm_exercise_form_agent(state, llm))
+
     workflow.add_node("exercise_planning", lambda state: planning(state, llm))
+    workflow.add_node("exercise_execute", lambda state: execute_plan(state, llm))
 
     # 엣지 추가
     # workflow.add_edge(START, "routing")
@@ -43,6 +46,7 @@ def create_workout_workflow():
     # workflow.add_edge("exercise_routine", END)
 
     workflow.add_edge(START, "exercise_planning")
-    workflow.add_edge("exercise_planning", END)
+    workflow.add_edge("exercise_planning", "exercise_execute")
+    workflow.add_edge("exercise_execute", END)
     
     return workflow.compile()
