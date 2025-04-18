8b9f5bc424f6ef29e9d332ad34cfccf3f8178175
JIHYE <jihyeoh91@gmail.com>
2025-04-18 18:49:14 +0900

    feat: agent_supervisor 구조

---
+++ trainer_chat/supervisor_node.py
@@ -0,0 +1,44 @@
+from typing import Literal
+from typing_extensions import TypedDict
+
+from langgraph.graph import MessagesState, END
+from langgraph.types import Command
+from langchain_openai import ChatOpenAI
+
+members = ["researcher"]
+# Our team supervisor is an LLM node. It just picks the next agent to process
+# and decides when the work is completed
+options = members + ["FINISH"]
+
+system_prompt = (
+    "You are a supervisor tasked with managing a conversation between the"
+    f" following workers: {members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH."
+)
+
+
+class Router(TypedDict):
+    """Worker to route to next. If no workers needed, route to FINISH."""
+
+    next: Literal[*options]
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.56)
+
+
+class State(MessagesState):
+    next: str
+
+
+def supervisor_node(state: State) -> Command[Literal[*members, "__end__"]]:
+    messages = [
+        {"role": "system", "content": system_prompt},
+    ] + state["messages"]
+    response = llm.with_structured_output(Router).invoke(messages)
+    goto = response["next"]
+    if goto == "FINISH":
+        goto = END
+
+    return Command(goto=goto, update={"next": goto})
