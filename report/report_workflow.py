d1dca430ae713e91515abb08214d491c8e2368ad
jihxn-kim <lizars4024@gmail.com>
2025-04-21 12:53:00 +0900

    Feat: 인바디 기록 리포트 작성하는 노드 추가

--- report/report_workflow.py
+++ report/report_workflow.py
@@ -1,7 +1,7 @@
 from langgraph.graph import StateGraph, END, START
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from report.report_exercise_node import analyze_pt_log, add_data
+from report.report_exercise_node import analyze_pt_log, add_data, analyze_inbody_data
 from report.report_model import reportState
 
 load_dotenv()
@@ -16,10 +16,12 @@ def create_report_workflow():
     workflow = StateGraph(reportState)
 
     workflow.add_node("analyze_exercise_report", lambda state: analyze_pt_log(state, llm))
+    workflow.add_node("analyze_inbody_report", lambda state: analyze_inbody_data(state, llm))
     workflow.add_node("add_data", lambda state: add_data(state, llm))
 
     workflow.add_edge(START, "analyze_exercise_report")
-    workflow.add_edge("analyze_exercise_report", "add_data")
+    workflow.add_edge("analyze_exercise_report", "analyze_inbody_report")
+    workflow.add_edge("analyze_inbody_report", "add_data")
     workflow.add_edge("add_data", END)
 
     result = workflow.compile()
