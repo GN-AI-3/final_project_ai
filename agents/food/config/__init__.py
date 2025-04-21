96cb1ab3d6f561d281ee7bbaca03c52243702405
KimSe-hyuk <rlatpgur627@gmail.com>
2025-04-21 18:41:50 +0900

    fodo

--- agents/food/config/__init__.py
+++ agents/food/config/__init__.py
@@ -1,4 +1,4 @@
 from .database_config import PG_URI
-from .api_config import API_BASE_URL, AUTH_TOKEN, EC2_BACKEND_URL
+from .api_config import  AUTH_TOKEN, EC2_BACKEND_URL
 
-__all__ = ['PG_URI', 'API_BASE_URL', 'AUTH_TOKEN', 'EC2_BACKEND_URL'] 
+__all__ = ['PG_URI' , 'AUTH_TOKEN', 'EC2_BACKEND_URL'] 
