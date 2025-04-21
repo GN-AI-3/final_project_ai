f9688da515f3bbf0a665beaf84a415b896128172
Zara8170 <wlsgusan123@gmail.com>
2025-04-21 14:18:24 +0900

    timeout 시간 수정

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "[start.sh] ✅ Qdrant 분석기 실행 시작"
 python -m qdrant_utils.data_analyzer --mode schedule &
 
 echo "[start.sh] ✅ FastAPI 서버 실행 시작"
-gunicorn -k uvicorn.workers.UvicornWorker --workers 2 --bind 0.0.0.0:8000 api_server:app
+gunicorn -k uvicorn.workers.UvicornWorker --workers 2 --timeout 120 --bind 0.0.0.0:8000 api_server:app
