e7795152d64d2164bbb2b9a181a91969125a05e1
RJANG <150069439+R-JANG-98@users.noreply.github.com>
2025-04-28 12:24:52 +0900

    Update database_config.py

--- agents/schedule/config/database_config.py
+++ agents/schedule/config/database_config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 데이터베이스 설정
-DB_HOST = os.getenv("DB_HOST", "3.37.8.185")
-DB_PORT = os.getenv("DB_PORT", "5433")  # 기본 포트 번호 5432 사용
-DB_DB = os.getenv("DB_DB", "gym")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")  # 기본 포트 번호 5432 사용
+DB_DB = os.getenv("DB_DB")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # 데이터베이스 URI
 PG_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DB}" 
